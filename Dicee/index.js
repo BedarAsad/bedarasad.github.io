51bc17230801321deeee6e747a7d3d310c249015
BedarAsad <44999643+BedarAsad@users.noreply.github.com>
2021-01-15 18:53:10 +0600

    Update index.js
    bug fixing

--- Dicee/index.js
+++ Dicee/index.js
@@ -1,6 +1,6 @@
-var player1 =  Math.floor(Math.random()*7);
+var player1 =  Math.floor(Math.random()*6) + 1;
 
-var player2 = Math.floor(Math.random()*7);
+var player2 = Math.floor(Math.random()*6) + 1;
 
 if(player1 > player2){
   document.querySelector("h1").innerHTML = "Player 1 wins!";
