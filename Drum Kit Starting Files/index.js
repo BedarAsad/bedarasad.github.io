45caf37efa742775288435a944b32219921a7c7e
BedarAsad <44999643+BedarAsad@users.noreply.github.com>
2021-01-20 23:49:10 +0600

    Simple drum application

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,68 @@
+
+var numberOfButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i<numberOfButtons; i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    var buttonCode = this.innerHTML;
+    makeSound(buttonCode);
+    buttonAnimation(buttonCode);
+  });
+}
+
+document.addEventListener("keypress", function(event){
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+function makeSound(key){
+  switch (key) {
+    case "w":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+
+    break;
+
+    case "a":
+      var crash = new Audio("sounds/kick-bass.mp3");
+      crash.play();
+
+    break;
+    case "s":
+      var crash = new Audio("sounds/snare.mp3");
+      crash.play();
+
+    break;
+    case "d":
+      var crash = new Audio("sounds/tom-1.mp3");
+      crash.play();
+
+    break;
+    case "j":
+      var crash = new Audio("sounds/tom-2.mp3");
+      crash.play();
+
+    break;
+    case "k":
+      var crash = new Audio("sounds/tom-3.mp3");
+      crash.play();
+
+    break;
+    case "l":
+      var crash = new Audio("sounds/tom-4.mp3");
+      crash.play();
+
+    break;
+
+    default:
+  }
+}
+
+function buttonAnimation(currentKey){
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  }, 100);
+
+  }
